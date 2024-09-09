e1cbffcaff34a0684ccf250770626d3b12c86b04
Ferrari78 <88674950+Ferrari78@users.noreply.github.com>
2024-09-09 11:52:43 +0200

    rename directories and files

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,7 +4,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'SplashScreen/login_view.dart';
 import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
