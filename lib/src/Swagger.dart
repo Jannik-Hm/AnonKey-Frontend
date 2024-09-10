e307479d62742ce33c95a536e44ec821f9e5bfa7
Ferrari78 <88674950+Ferrari78@users.noreply.github.com>
2024-09-10 16:32:22 +0200

    init openapi

---
+++ lib/src/Swagger.dart
@@ -0,0 +1,14 @@
+// Openapi Generator last run: : 2024-09-10T16:27:03.061825
+import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
+
+@Openapi(
+  additionalProperties: AdditionalProperties(
+    pubName: 'anonkey_frontend',
+    pubAuthor: 'Anonkey',
+    pubDescription: 'Anonkey frontend',
+  ),
+  inputSpec: InputSpec(path: 'openapi/swagger.yaml'),
+  generatorName: Generator.dart,
+  outputDirectory: 'lib/api',
+)
+class Swagger {}
