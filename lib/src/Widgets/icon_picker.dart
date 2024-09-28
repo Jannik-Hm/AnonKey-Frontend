2af6acf9f9d30e9537cef01b965c6b4bb0507c10
Agent-Husky <jannik.heym@gmx.de>
2024-09-28 18:23:41 +0200

    added central entry_input and icon_picker widgets

---
+++ lib/src/Widgets/icon_picker.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_iconpicker/flutter_iconpicker.dart' as iconpicker;
+import 'package:flutter_iconpicker/Models/configuration.dart';
+
+class IconPicker extends StatefulWidget {
+  //final TextEditingController controller;
+  //final String label;
+  //final VoidCallback? onEnterPressed;
+  final void Function({required IconData? iconData})? iconCallback;
+  //final FocusNode focus;
+  final bool enabled;
+  final IconData? iconData;
+
+  const IconPicker({
+    super.key,
+    //required this.controller,
+    //required this.label,
+    //this.onEnterPressed,
+    //required this.focus,
+    this.iconCallback,
+    this.enabled = true,
+    this.iconData,
+  });
+
+  @override
+  State<StatefulWidget> createState() => _IconPicker();
+}
+
+class _IconPicker extends State<IconPicker> {
+  late bool _enabled;
+  IconData? _iconData;
+
+  _pickIcon() async {
+    iconpicker.IconPickerIcon? icon = await iconpicker.showIconPicker(
+      context,
+      configuration: SinglePickerConfiguration(
+        iconPackModes: [iconpicker.IconPack.material],
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        iconColor: Theme.of(context).colorScheme.onSurface,
+      ),
+    );
+    _iconData = icon?.data;
+    setState(() {
+      if(widget.iconCallback != null){
+        widget.iconCallback!(iconData: icon?.data);
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the mutable object from the widget field
+    _enabled = widget.enabled;
+    _iconData = widget.iconData;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 20.0, top: 10.0, right: 20.0, bottom: 10.0),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.all(Radius.circular(25)),
+        color: Theme.of(context).colorScheme.secondary,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            child: (_iconData != null)
+                ? Icon(
+                    _iconData,
+                    color: Theme.of(context).colorScheme.onSurface,
+                    size: 40.0,
+                  )
+                : Container(),
+          ),
+          const SizedBox(width: 20.0),
+          if (_enabled)
+            TextButton(
+              onPressed: _pickIcon,
+              style: TextButton.styleFrom(backgroundColor: Theme.of(context).colorScheme.onPrimary),
+              child: const Text("Icon auswählen"),
+            )
+        ],
+      ),
+    );
+  }
+}
