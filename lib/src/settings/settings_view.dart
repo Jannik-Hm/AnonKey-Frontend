7a8de68544f71022bc45356551866f9b9c9597a1
Nessi <88674950+Ferrari78@users.noreply.github.com>
2024-10-13 17:21:07 +0200

    fix merge conflicts

--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -17,7 +17,7 @@ class SettingsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    void _logout() async {
+    void logout() async {
       await AuthService.deleteAuthenticationCredentials();
       if (!context.mounted) return;
       GoRouter.of(context).clearStackAndNavigate("login");
@@ -53,203 +53,208 @@ class SettingsView extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-           Text(
-            'Theme',
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-                color: theme.colorScheme.onSurface
+          children: [
+            Text(
+              'Theme',
+              style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: theme.colorScheme.onSurface),
             ),
-          ),
-          const SizedBox(height: 8),
-          ListTile(
-            leading: Icon(Icons.brightness_6, color: theme.colorScheme.onSurface),
-            title: Text(AppLocalizations.of(context)!.selectTheme, style: TextStyle(color: theme.colorScheme.onSurface)),
-            trailing: DropdownButton<ThemeMode>(
-              value: controller.themeMode,
-              onChanged: (ThemeMode? newTheme) =>
-                  controller.updateThemeMode(newTheme, context),
-              items: [
-                DropdownMenuItem(
-                  value: ThemeMode.system,
-                  child: Text(AppLocalizations.of(context)!.systemTheme),
-                ),
-                DropdownMenuItem(
-                  value: ThemeMode.light,
-                  child: Text(AppLocalizations.of(context)!.lightTheme),
-                ),
-                DropdownMenuItem(
-                  value: ThemeMode.dark,
-                  child: Text(AppLocalizations.of(context)!.darkTheme),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 8),
-          ListTile(
-            leading: Icon(Icons.language, color: theme.colorScheme.onSurface),
-            title: Text(AppLocalizations.of(context)!.language, style: TextStyle(color: theme.colorScheme.onSurface)),
-            trailing: DropdownButton<Locale>(
-              value: controller.languageMode,
-              onChanged: (Locale? newLanguage) =>
-                  controller.updateLanguage(newLanguage!),
-              items: [
-                DropdownMenuItem(
-                  value: const Locale("de"),
-                  child: Text(AppLocalizations.of(context)!.german),
-                ),
-                DropdownMenuItem(
-                  value: const Locale("en"),
-                  child: Text(AppLocalizations.of(context)!.english),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            AppLocalizations.of(context)!.openSourceLicenses,
-            style: TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: theme.colorScheme.onSurface,
-            ),
-          ),
-          const SizedBox(height: 8),
-          ListTile(
-            leading: Icon(Icons.info, color: theme.colorScheme.onSurface),
-            title: Text(AppLocalizations.of(context)!.viewLicenses, style: TextStyle(color: theme.colorScheme.onSurface)),
-            onTap: () => showLicensePage(context: context),
-          ),
-          const SizedBox(height: 20),
-          ValueListenableBuilder<bool>(
-            valueListenable: controller.isBiometricEnabled,
-            builder: (context, isBiometricEnabled, child) {
-              return SwitchListTile(
-                title:
-                    Text(AppLocalizations.of(context)!.biometricAuthentication),
-                value: isBiometricEnabled,
-                onChanged: (bool value) {
-                  controller.updateBiometricSetting(value);
-                },
-              );
-            },
-          ),
-          const SizedBox(height: 20),
-          Center(
-            child: ElevatedButton.icon(
-              onPressed: logout,
-              icon: const Icon(Icons.logout),
-              label: Text(AppLocalizations.of(context)!.logout),
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                backgroundColor: Theme.of(context).colorScheme.primary,
+            const SizedBox(height: 8),
+            ListTile(
+              leading:
+                  Icon(Icons.brightness_6, color: theme.colorScheme.onSurface),
+              title: Text(AppLocalizations.of(context)!.selectTheme,
+                  style: TextStyle(color: theme.colorScheme.onSurface)),
+              trailing: DropdownButton<ThemeMode>(
+                value: controller.themeMode,
+                onChanged: (ThemeMode? newTheme) =>
+                    controller.updateThemeMode(newTheme, context),
+                items: [
+                  DropdownMenuItem(
+                    value: ThemeMode.system,
+                    child: Text(AppLocalizations.of(context)!.systemTheme),
+                  ),
+                  DropdownMenuItem(
+                    value: ThemeMode.light,
+                    child: Text(AppLocalizations.of(context)!.lightTheme),
+                  ),
+                  DropdownMenuItem(
+                    value: ThemeMode.dark,
+                    child: Text(AppLocalizations.of(context)!.darkTheme),
+                  ),
+                ],
               ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Center(
-            child: ElevatedButton.icon(
-              onPressed: () => {
-                showModalBottomSheet<void>(
-                  context: context,
-                  isScrollControlled: true,
-                  builder: (BuildContext context) {
-                    bool isDarkTheme;
-                    if (ThemeMode.system != controller.themeMode) {
-                      isDarkTheme =
-                          controller.themeMode == ThemeMode.dark ? true : false;
-                    } else {
-                      var brightness =
-                          MediaQuery.of(context).platformBrightness;
-                      isDarkTheme = brightness == Brightness.dark;
-                    }
-                    return ClipRRect(
-                      borderRadius:
-                          const BorderRadius.vertical(top: Radius.circular(20)),
-                      child: Container(
-                        color: isDarkTheme
-                            ? ThemeData.dark().scaffoldBackgroundColor
-                            : ThemeData.light().scaffoldBackgroundColor,
-                        padding: EdgeInsets.only(
-                          bottom: MediaQuery.of(context).viewInsets.bottom,
-                          left: 16,
-                          right: 16,
-                          top: 16,
-                        ),
-                        child: SingleChildScrollView(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Text(
-                                AppLocalizations.of(context)!.deleteUser,
-                                style: const TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
+            const SizedBox(height: 8),
+            ListTile(
+              leading: Icon(Icons.language, color: theme.colorScheme.onSurface),
+              title: Text(AppLocalizations.of(context)!.language,
+                  style: TextStyle(color: theme.colorScheme.onSurface)),
+              trailing: DropdownButton<Locale>(
+                value: controller.languageMode,
+                onChanged: (Locale? newLanguage) =>
+                    controller.updateLanguage(newLanguage!),
+                items: [
+                  DropdownMenuItem(
+                    value: const Locale("de"),
+                    child: Text(AppLocalizations.of(context)!.german),
+                  ),
+                  DropdownMenuItem(
+                    value: const Locale("en"),
+                    child: Text(AppLocalizations.of(context)!.english),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              AppLocalizations.of(context)!.openSourceLicenses,
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: theme.colorScheme.onSurface,
+              ),
+            ),
+            const SizedBox(height: 8),
+            ListTile(
+              leading: Icon(Icons.info, color: theme.colorScheme.onSurface),
+              title: Text(AppLocalizations.of(context)!.viewLicenses,
+                  style: TextStyle(color: theme.colorScheme.onSurface)),
+              onTap: () => showLicensePage(context: context),
+            ),
+            const SizedBox(height: 20),
+            ValueListenableBuilder<bool>(
+              valueListenable: controller.isBiometricEnabled,
+              builder: (context, isBiometricEnabled, child) {
+                return SwitchListTile(
+                  title: Text(
+                      AppLocalizations.of(context)!.biometricAuthentication),
+                  value: isBiometricEnabled,
+                  onChanged: (bool value) {
+                    controller.updateBiometricSetting(value);
+                  },
+                );
+              },
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: logout,
+                icon: const Icon(Icons.logout),
+                label: Text(AppLocalizations.of(context)!.logout),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  backgroundColor: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: () => {
+                  showModalBottomSheet<void>(
+                    context: context,
+                    isScrollControlled: true,
+                    builder: (BuildContext context) {
+                      bool isDarkTheme;
+                      if (ThemeMode.system != controller.themeMode) {
+                        isDarkTheme = controller.themeMode == ThemeMode.dark
+                            ? true
+                            : false;
+                      } else {
+                        var brightness =
+                            MediaQuery.of(context).platformBrightness;
+                        isDarkTheme = brightness == Brightness.dark;
+                      }
+                      return ClipRRect(
+                        borderRadius: const BorderRadius.vertical(
+                            top: Radius.circular(20)),
+                        child: Container(
+                          color: isDarkTheme
+                              ? ThemeData.dark().scaffoldBackgroundColor
+                              : ThemeData.light().scaffoldBackgroundColor,
+                          padding: EdgeInsets.only(
+                            bottom: MediaQuery.of(context).viewInsets.bottom,
+                            left: 16,
+                            right: 16,
+                            top: 16,
+                          ),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  AppLocalizations.of(context)!.deleteUser,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(height: 8),
-                              TextField(
-                                controller: controller.password,
-                                decoration: InputDecoration(
-                                  labelText:
-                                      AppLocalizations.of(context)!.password,
+                                const SizedBox(height: 8),
+                                TextField(
+                                  controller: controller.password,
+                                  decoration: InputDecoration(
+                                    labelText:
+                                        AppLocalizations.of(context)!.password,
+                                  ),
+                                  obscureText: true,
                                 ),
-                                obscureText: true,
-                              ),
-                              const SizedBox(height: 20),
-                              ValueListenableBuilder<String?>(
-                                valueListenable: controller.errorMessage,
-                                builder: (context, errorMessage, child) {
-                                  return errorMessage != null
-                                      ? Align(
-                                          alignment: Alignment.centerLeft,
-                                          child: Text(
-                                            errorMessage,
-                                            style: const TextStyle(
-                                                color: Colors.red),
-                                          ),
-                                        )
-                                      : Container();
-                                },
-                              ),
-                              const SizedBox(height: 20),
-                              ValueListenableBuilder<bool>(
-                                valueListenable: controller.isPasswordEmpty,
-                                builder: (context, isPasswordEmpty, child) {
-                                  return ElevatedButton(
-                                    onPressed:
-                                        isPasswordEmpty ? null : deleteUser,
-                                    style: ElevatedButton.styleFrom(
-                                      foregroundColor: Theme.of(context)
-                                          .colorScheme
-                                          .onPrimary,
-                                      backgroundColor:
-                                          Theme.of(context).colorScheme.error,
-                                    ),
-                                    child: Text(AppLocalizations.of(context)!
-                                        .deleteUser),
-                                  );
-                                },
-                              ),
-                            ],
+                                const SizedBox(height: 20),
+                                ValueListenableBuilder<String?>(
+                                  valueListenable: controller.errorMessage,
+                                  builder: (context, errorMessage, child) {
+                                    return errorMessage != null
+                                        ? Align(
+                                            alignment: Alignment.centerLeft,
+                                            child: Text(
+                                              errorMessage,
+                                              style: const TextStyle(
+                                                  color: Colors.red),
+                                            ),
+                                          )
+                                        : Container();
+                                  },
+                                ),
+                                const SizedBox(height: 20),
+                                ValueListenableBuilder<bool>(
+                                  valueListenable: controller.isPasswordEmpty,
+                                  builder: (context, isPasswordEmpty, child) {
+                                    return ElevatedButton(
+                                      onPressed:
+                                          isPasswordEmpty ? null : deleteUser,
+                                      style: ElevatedButton.styleFrom(
+                                        foregroundColor: Theme.of(context)
+                                            .colorScheme
+                                            .onPrimary,
+                                        backgroundColor:
+                                            Theme.of(context).colorScheme.error,
+                                      ),
+                                      child: Text(AppLocalizations.of(context)!
+                                          .deleteUser),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  },
+                      );
+                    },
+                  ),
+                },
+                icon: const Icon(Icons.delete),
+                label: Text(AppLocalizations.of(context)!.deleteUser),
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Theme.of(context).colorScheme.onPrimary,
+                  backgroundColor: Theme.of(context).colorScheme.error,
                 ),
-              },
-              icon: const Icon(Icons.delete),
-              label: Text(AppLocalizations.of(context)!.deleteUser),
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Theme.of(context).colorScheme.onPrimary,
-                backgroundColor: Theme.of(context).colorScheme.error,
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
