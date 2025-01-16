157452dd82dffc2c4aa128c43b343e5290cf5d10
Nessi <88674950+Ferrari78@users.noreply.github.com>
2025-01-16 16:18:57 +0100

    Swagger api

--- lib/src/swagger.dart
+++ lib/src/swagger.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2024-10-09T13:13:06.406307
+// Openapi Generator last run: : 2025-01-16T16:17:19.339407
 import 'package:openapi_generator_annotations/openapi_generator_annotations.dart';
 
 @Openapi(
