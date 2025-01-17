41e534fe81f43cfc0d412439e172cc930c407c9a
Agent-Husky <jannik.heym@gmx.de>
2025-01-18 00:08:03 +0100

    added ApiBaseData as a central location for static funcs like ApiWrapper, get Url, etc.

---
+++ lib/Utility/api_base_data.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'dart:developer';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class ApiBaseData {
+  static Future<String?> getURL() async {
+    return await SharedPreferencesAsync().getString('url') ?? "";
+  }
+
+  static Future<void> setURL(String url) async {
+    await SharedPreferencesAsync().setString('url', url);
+  }
+
+  static Future<bool?> lastCallSuccessful() async {
+    return await SharedPreferencesAsync().getBool('lastapicallsuccess');
+  }
+
+  static Future<void> setlastCallSuccessful(bool boolean) async {
+    await SharedPreferencesAsync()
+        .setBool('lastapicallsuccess', boolean);
+  }
+
+  static Future<T?> apiCallWrapper<T>(Future<T?> asyncFunction,
+      {required String logMessage, Duration? timeout}) async {
+    try {
+      timeout ??= Duration(seconds: 5);
+      T? result = await asyncFunction.timeout(timeout);
+      return result;
+    } catch (e) {
+      if (e is TimeoutException) {
+        log("TimeoutException: $logMessage");
+        await setlastCallSuccessful(false);
+        return null;
+      }
+      rethrow;
+    }
+  }
+}
